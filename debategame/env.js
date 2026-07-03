20cb51c347911478bef405d10510c705f4692382
universe <jiwhan.park17@gmail.com>
2026-07-03 22:12:10 +0900

    Add files via upload

---
+++ debategame/env.js
@@ -0,0 +1,8 @@
+window.env = {
+  apiKey: "AIzaSyCxGC-Y1IEWDS5AwjoyzN3RcodJXWGwJGA",
+  authDomain: "debate-game-f0f6b.firebaseapp.com",
+  projectId: "debate-game-f0f6b",
+  storageBucket: "debate-game-f0f6b.firebasestorage.app",
+  messagingSenderId: "373813942621",
+  appId: "1:373813942621:web:d632ff886f52d2b5e1b0b4"
+};
